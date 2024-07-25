aa56a214ffcf39d99b385cb5e4cb788b2d79d70b
Sensei <noppongsen@gmail.com>
2024-07-25 19:37:34 +0700

    1

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -21,16 +21,21 @@ export default function Task({
     deleteTaskFunc(id);
   };
 
+  const doneBtnOnClick = () => {
+    toggleDoneTaskFunc(id);
+  };
+
   return (
     <div className="d-flex p-3 gap-2 align-items-center border-bottom">
-      {/*
-      HINTS: if task is completed, below "span" will show like this 
-        <span className="text-decoration-line-through">{title}</span>
-        But if task is not completed : 
+      {/* Conditionally render task title */}
+      {completed ? (
+        <span style={{ textDecoration: 'line-through' }}>{title}</span>
+      ) : (
         <span>{title}</span>
-      */}
-      <span>{title}</span>
-      <button className="btn btn-success">Done</button>
+      )}
+      <button className="btn btn-success" onClick={doneBtnOnClick}>
+        Done
+      </button>
       <button className="btn btn-danger" onClick={deleteBtnOnClick}>
         Delete
       </button>
